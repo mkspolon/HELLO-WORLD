418d92658b39c52c620349112f381303aa2977c4
mkspolon <94297138+mkspolon@users.noreply.github.com>
2021-11-19 10:05:56 +0000

    task1

--- rolling_the_dip.py
+++ rolling_the_dip.py
@@ -1,1 +1,13 @@
-#coment
+import random
+print("Dice Rolling Game")
+user_input = []
+while user_input !='yes' or user_input !='y':
+    user_input = (input("Want to Roll (y/n):"))
+
+    if user_input == 'y':
+        d1=random.randint(1,6)
+        print(d1)
+    else:
+        print("end of the game")
+        break
+    
